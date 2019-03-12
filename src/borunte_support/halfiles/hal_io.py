ef41b33b9a0702f5af0ecd01541f1ccffe972244
Alexander Rössler <alex@machinekoder.com>
2019-03-12 20:06:18 +0000

    hal_io: increase timeout

--- src/borunte_support/halfiles/hal_io.py
+++ src/borunte_support/halfiles/hal_io.py
@@ -5,7 +5,7 @@ from machinekit import hal
 def load_hal_io_component():
     # Load hal_io user component
     cmd = 'rosrun hal_hw_interface hal_io'
-    hal.loadusr(cmd, wait=True, wait_name='hal_io')
+    hal.loadusr(cmd, wait=True, wait_name='hal_io', wait_timeout=10.0)
 
 
 load_hal_io_component()
